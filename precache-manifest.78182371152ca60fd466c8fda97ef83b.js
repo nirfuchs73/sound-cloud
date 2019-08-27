119c3be50b8c21130ff364085d88441a943b7235
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-08-27 09:42:19 +0300

    Updates

---
+++ precache-manifest.78182371152ca60fd466c8fda97ef83b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f5c69030ac79d7ceb3ce7d8e8145c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cef8ec573ad19720efa8",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "3a736fba2c49156c4751",
+    "url": "/static/js/2.d2c28d9d.chunk.js"
+  },
+  {
+    "revision": "cef8ec573ad19720efa8",
+    "url": "/static/js/main.f9d33b18.chunk.js"
+  },
+  {
+    "revision": "288aed0f65919be2ad0b",
+    "url": "/static/js/runtime~main.2739bb68.js"
+  }
+]);
