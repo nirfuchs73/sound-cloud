259edf65e3c3ab8c56ee8ee101ae4c70abca0d35
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-08-27 23:02:51 +0300

    Provider

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Switch, NavLink, Redirect } from 'react-router-dom';
 // import './App.css';
 import classes from './App.module.css';
-import store from './store';
+// import store from './store';
 import SearchPage from './containers/SearchPage/SearchPage';
 import PlayPage from './containers/PlayPage/PlayPage';
 import HistoryPage from './containers/HistoryPage/HistoryPage';
@@ -16,12 +16,12 @@ function App() {
         <NavLink to="/history">History</NavLink>
       </nav>
       <Switch>
-        {/* <Route path="/search" component={SearchPage} /> */}
-        <Route path='/search' render={(props) => <SearchPage store={store} {...props} />} />
-        {/* <Route path="/play" component={PlayPage} /> */}
-        <Route path='/play' render={(props) => <PlayPage store={store} {...props} />} />
-        {/* <Route path="/history" component={HistoryPage} /> */}
-        <Route path='/history' render={(props) => <HistoryPage store={store} {...props} />} />
+        <Route path="/search" component={SearchPage} />
+        <Route path="/play" component={PlayPage} />
+        <Route path="/history" component={HistoryPage} />
+        {/* <Route path='/search' render={(props) => <SearchPage store={store} {...props} />} /> */}
+        {/* <Route path='/play' render={(props) => <PlayPage store={store} {...props} />} /> */}
+        {/* <Route path='/history' render={(props) => <HistoryPage store={store} {...props} />} /> */}
         {/* <Route render={() => <h1>404 Error - Page Not found</h1>} /> */}
         <Redirect from="/" to="/search" />
       </Switch>
