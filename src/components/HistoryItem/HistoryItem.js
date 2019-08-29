17f871661cb5744d2c60c13cfe42f25fec3290b7
nirfuchs\fuchs <nirfuchs@hotmail.com>
2019-08-29 16:30:10 +0300

    style

--- src/components/HistoryItem/HistoryItem.js
+++ src/components/HistoryItem/HistoryItem.js
@@ -5,7 +5,8 @@ import classes from './HistoryItem.module.css';
 const historyItem = (props) => {
   return (
     <div className={classes.HistoryItem} onClick={props.historyItemClicked}>
-      <div>{props.title}</div>
+      <i className="fas fa-search" ></i>
+      <div className={classes.Text}>{props.title}</div>
     </div>
   );
 }
--- src/components/Result/Result.js
+++ src/components/Result/Result.js
@@ -5,7 +5,8 @@ import classes from './Result.module.css';
 const result = (props) => {
   return (
     <div className={classes.Result} onClick={props.resultClicked}>
-      <div>{props.title}</div>
+      <i className="fas fa-play"></i>
+      <div className={classes.Text}>{props.title}</div>
     </div>
   );
 }
